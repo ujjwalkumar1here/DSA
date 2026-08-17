909c02e17ea41b239219256d19cc0dfd245fddf4
ujjwal1here <ujjwalrajput.here@gmail.com>
2026-08-17 21:04:37 +0530

    Add fibonacci code

---
+++ Recursion/fibonacci.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n)
+{
+    // Base case
+    if(n == 0)
+        return 0;
+
+    if(n == 1)
+        return 1;
+
+  
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << fibonacci(i) << " ";
+    }
+
+    return 0;
+}
